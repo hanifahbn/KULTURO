a42890c4ed8587782684d39a00cdbf36556e02da
Hanifah BN <ifhahnfh07@gmail.com>
2023-10-17 16:21:15 +0700

    Initial Commit

---
+++ MacroApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacroApp
+//
+//  Created by Hanifah BN on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacroApp/MacroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroAppApp.swift
+//  MacroApp
+//
+//  Created by Hanifah BN on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
