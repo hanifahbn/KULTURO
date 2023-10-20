dfc8982daed0b7e0ee2482beb69c43246876dc17
hanifahbn <50043939+hanifahbn@users.noreply.github.com>
2023-10-20 16:45:13 +0700

    Revert "GameKIt, Slicing ScreenMenu"

--- MacroApp/ContentView.swift
+++ MacroApp/ContentView.swift
@@ -2,28 +2,23 @@
 //  ContentView.swift
 //  MacroApp
 //
-//  Created by Irvan P. Saragi on 18/10/23.
+//  Created by Hanifah BN on 17/10/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
-    @StateObject var matchManager = MatchManager()
     var body: some View {
-        Group{
-            switch matchManager.gameStatus {
-            case .setup:
-                onBoardView(matchManager: matchManager)
-            case .inGame:
-                InGameView()
-            case .gameOver:
-                EndGameView()
-            }
-        }.onAppear{
-            matchManager.authenticateUser()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
+
 #Preview {
     ContentView()
 }
