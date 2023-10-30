c326a824f83879f02f3eca7296ad921a3e70609e
irvan <pradaindo24@gmail.com>
2023-10-31 00:21:35 +0700

    GameKit Timer

--- MacroApp/MacroApp.swift
+++ MacroApp/MacroApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct MacroApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            DragDropView()
+            AyakPasirView()
         }
     }
 }
--- View/AyakPasirView.swift
+++ View/AyakPasirView.swift
@@ -1,11 +1,3 @@
-//
-//  AyakPasirView.swift
-//  MacroApp
-//
-//  Created by Irvan P. Saragi on 26/10/23.
-//
-
-
 import SwiftUI
 import CoreMotion
 
@@ -14,10 +6,41 @@ struct AyakPasirView: View {
     @State private var motionManager = CMMotionManager()
     @State private var flyCounter = 0
     @State private var isModalPresented = false
+    @State private var elapsedTime: TimeInterval = 0
+    @State private var isTimerRunning = false
+    let timerInterval = 1.0
+    
+    var timerText: String {
+        let minutes = Int(elapsedTime) / 60
+        let seconds = Int(elapsedTime) % 60
+        return String(format: "%02d.%02d", minutes, seconds)
+    }
     
     var body: some View {
         ZStack {
             Image("Backgroundpasir")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                HStack{
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 16)
+                        .frame(width: 120, height: 54)
+                        .foregroundStyle(.white)
+                        .opacity(0.5)
+                        .overlay(content: {
+                            HStack{
+                                Image(systemName: "timer")
+                                    .font(.system(size: 32, weight: .bold))
+                                Text(timerText)
+                                    .font(.system(size: 19, weight: .bold))
+                            }
+                        })
+                }
+                Spacer()
+            }
+            .padding(40)
             ZStack {
                 Image("Ayak")
                     .resizable()
@@ -31,7 +54,8 @@ struct AyakPasirView: View {
                     .animation(.bouncy(duration: 1), value: isFlying)
                     .padding(.bottom, 50)
             }
-            if flyCounter >= 10 {
+            .padding(.top, 50)
+            if flyCounter > 12 && elapsedTime == 30 {
                 Image("PasirAkhir")
                     .resizable()
                     .frame(width: 400, height: 900)
@@ -46,22 +70,36 @@ struct AyakPasirView: View {
         }
         .onAppear {
             startMotionUpdates()
+            startTimer()
         }
     }
-
+    func startTimer() {
+        isTimerRunning = true
+        Timer.scheduledTimer(withTimeInterval: timerInterval, repeats: true) { timer in
+            elapsedTime += timerInterval
+            if elapsedTime >= 20 && flyCounter == 0 {
+                timer.invalidate()
+                isTimerRunning = false
+                print("kamu gagal")
+            } else if flyCounter > 1 && elapsedTime == 50{
+                print("kamu berhasil")
+                isTimerRunning = false
+            }
+        }
+    }
+    
     func startMotionUpdates() {
         motionManager.accelerometerUpdateInterval = 0.1
         motionManager.startAccelerometerUpdates(to: .main) { accelerometerData, error in
             if let acceleration = accelerometerData?.acceleration {
-                let shakeThreshold: Double = 1.2
+                let shakeThreshold: Double = 2
                 if abs(acceleration.x) > shakeThreshold || abs(acceleration.y) > shakeThreshold || abs(acceleration.z) > shakeThreshold {
                     // Device is shaken, trigger the fly animation
                     isFlying = true
                     flyCounter += 1
-                    if flyCounter >= 10 {
+                    if flyCounter >= 100 {
                         // Disable onAppear after 10 times
                         self.motionManager.stopAccelerometerUpdates()
-                        isModalPresented = true
                     }
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                         isFlying = false
@@ -94,3 +132,4 @@ struct ModalPasirView: View {
         }
     }
 }
+
