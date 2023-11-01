c6ad89282f8482e3d7ede2050667f66322cb4d80
Hanifah BN <ifhahnfh07@gmail.com>
2023-11-01 08:49:53 +0700

    Fixing audio files + change mode to unreliable.

--- MatchManager/MatchManager+Synchronizations.swift
+++ MatchManager/MatchManager+Synchronizations.swift
@@ -11,19 +11,19 @@ import GameKit
 extension MatchManager{
     func synchronizeCharacterSelection(_ selectedCharacter: Karakter) {
         if let encodedData = try? JSONEncoder().encode(selectedCharacter) {
-            sendData(encodedData, mode: .reliable)
+            sendData(encodedData, mode: .unreliable)
         }
     }
     
     func synchronizeGameState(_ updatedState: String) {
         if let encodedData = updatedState.data(using: .utf8) {
-            sendData(encodedData, mode: .reliable)
+            sendData(encodedData, mode: .unreliable)
         }
     }
     
     func synchronizeGameCharacters(_ characters: [Karakter]) {
         if let encodedData = try? JSONEncoder().encode(characters) {
-            sendData(encodedData, mode: .reliable)
+            sendData(encodedData, mode: .unreliable)
         }
     }
 }
