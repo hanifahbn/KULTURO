c586c58b311e1d5ed383f70a3eaceed9267049c3
Hanifah BN <ifhahnfh07@gmail.com>
2023-11-09 10:32:25 +0700

    Coloring mission one text with dark red.

--- View/Mini Games/DummyView.swift
+++ View/Mini Games/DummyView.swift
@@ -1,39 +1,19 @@
+//
+//  DummyView.swift
+//  MacroApp
+//
+//  Created by Hanifah BN on 09/11/23.
+//
+
 import SwiftUI
 
 struct DummyView: View {
     @EnvironmentObject var matchManager: MatchManager
-    @State private var refreshTrigger = false
-    
-    @State var text = "helow"
-    
     var body: some View {
-        VStack {
-            Text("Tampilan SwiftUI yang ingin Anda atur ulang")
-                .padding()
-
-            Text(text)
-                .padding()
-            
-            Button(action: {
-                self.refreshTrigger.toggle() // Ubah nilai untuk memicu pembaruan
-            }) {
-                Text("Atur Ulang Tampilan")
-            }
-        }
-        .onAppear {
-            text = "hai ini yah"
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                text = "berubah"
-                matchManager.gameStatus = .empty
-            }
-            matchManager.gameStatus = .dummy
-        }
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-
-
 #Preview {
     DummyView()
-        .environmentObject(MatchManager())
 }
